27bc2c6089021f8d5a31b058d3d73a741ce17e57
Milena Rubik Peres <56029188+milena-rubik@users.noreply.github.com>
2021-09-30 09:46:51 -0300

    Update 01VariavelConstante.js

--- Parte01/01VariavelConstante.js
+++ Parte01/01VariavelConstante.js
@@ -1,11 +1,11 @@
-const nome = 'Milena' //declara constante
-const sobrenome = 'Rubik Peres'
-const peso = 65.5
-const altura = 1.68
+const nome = 'Fulano' //declara constante
+const sobrenome = 'de Tal'
+const peso = 90
+const altura = 1.80
 const idade = 25
 let imc; //declara variável
-let anonascimentohttps://github.com/milena-rubik/EstudosEmJavaScript1/blob/master/Parte01/01VariavelConstane.js
+let anonascimento 
 imc = peso / (altura*altura)
 anonascimento = 2021 - 25
 console.log(nome, sobrenome,'nasceu em',anonascimento,'e tem IMC',imc) //função exibe mensagem no console
-console.log(`Ela tem ${idade} anos`) //outra forma de inserir variável numa string é iniciar com crase e colocar ${variavel}
+console.log(`Ele tem ${idade} anos`) //outra forma de inserir variável numa string é iniciar com crase e colocar ${variavel}
