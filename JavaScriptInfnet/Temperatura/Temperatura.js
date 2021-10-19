d1b98ca377f6f81313213bdd0264fef238832e6c
Milena Rubik Peres <56029188+milena-rubik@users.noreply.github.com>
2021-10-18 21:17:27 -0300

    Delete JavaScriptInfnet directory

--- JavaScriptInfnet/Temperatura/Temperatura.js
+++
@@ -1,3 +0,0 @@
-let fahrenheit = window.prompt('Informe a temperatura em graus Fahrenheit: ')
-let celsius = 5*(((parseFloat(fahrenheit))-32)/9)
-document.body.innerHTML += `A temperatura de ${fahrenheit} Fahrenheit em graus Celsius é de ${celsius.toFixed(2)}` //'.toFixed(x)' arredonda em x casas decimais
