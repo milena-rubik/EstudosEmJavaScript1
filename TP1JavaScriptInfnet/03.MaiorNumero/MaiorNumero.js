6b0aa0d2c36872c1b024a7c0ea1823a4ec3cf515
Milena Rubik Peres <56029188+milena-rubik@users.noreply.github.com>
2021-10-25 07:19:49 -0300

    Delete TP1JavaScriptInfnet directory

--- TP1JavaScriptInfnet/03.MaiorNumero/MaiorNumero.js
+++
@@ -1,8 +0,0 @@
-//3 – Faça um programa que peça dois números e imprima o maior deles.
-
-let numero1 = Number(window.prompt('Informe o primeiro número'))
-let numero2 = Number(window.prompt('Informe o segundo número'))
-
-if (numero1 > numero2) document.body.innerHTML = `O número ${numero1} é o maior`
-else if (numero1 < numero2) document.body.innerHTML = `O número ${numero2} é o maior`
-else document.body.innerHTML = `Os numeros informados são iguais`
--- TP1JavaScriptInfnet/04.PositivoNegativo/PositivoNegativo.js
+++
@@ -1,6 +0,0 @@
-let numero = Number(window.prompt('Informe um número'))
-
-if (numero>0) document.body.innerHTML = `O número informado é positivo`
-else if (numero<0) document.body.innerHTML = `O número informado é negativo`
-else document.body.innerHTML = `O número informado é neutro (0)`
-
