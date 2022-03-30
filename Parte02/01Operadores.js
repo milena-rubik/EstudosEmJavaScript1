d5b122af6effe855a9b040e8264a08288bc10cf1
Milena Rubik Peres <56029188+milena-rubik@users.noreply.github.com>
2022-03-30 10:21:21 -0300

    Add files via upload

---
+++ Parte02/01Operadores.js
@@ -0,0 +1,13 @@
+/*
+Operadores de comparação
+>= maior ou igual 
+<= menor ou igual
+== igualdade de valor - não recomendado porque vê apenas o valor da variável, então se eu tiver '10' string ou 10 number vai dizer que é igual / true.
+=== igualdade de valor e tipo
+!= diferente de valor - não recomendado porque vÊ apenas o valor, então '10' string e 10 number apresentará que não é diferente, ou seja, false.
+!== diferente estrito - valor e tipo
+*/
+
+const maiorigual = 10>=11
+console.log(maiorigual) // retorna false
+
