032e65032797dfa9236f418c0f6a7a8b0019f6e9
Milena Rubik Peres <56029188+milena-rubik@users.noreply.github.com>
2022-08-05 13:58:28 -0300

    Add files via upload

---
+++ Parte02/07For.js
@@ -0,0 +1,21 @@
+
+const doce = ['brigadeiro', 'cajuzinho', 'beijinho'];
+const bolo = {
+    recheio: 'dois-amores',
+    peso: 1,
+    decoracao: 'fazendinha'
+};
+
+//FOR 
+for (let i = 0; i < doce.length; i++){ //percorre todo o array 'doce'
+    console.log(doce[i]);
+}
+
+//FOR IN
+for (let i in doce){  //também percorre o vetor, detalhe que posso dar qualquer nome no lugar de i, exemplo 'let doces in doce'
+    console.log(doce[i]);
+}
+
+for (let escolha in bolo) {
+    console.log(escolha, bolo[escolha]); //exibe cada item do objeto, ex.: 'recheio dois amores'
+}
