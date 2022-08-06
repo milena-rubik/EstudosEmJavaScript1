83b662de6f99e922eb96d4aa9fdb27e01b1eccbf
Milena Rubik Peres <56029188+milena-rubik@users.noreply.github.com>
2022-08-06 12:30:07 -0300

    Add files via upload

---
+++ Parte02/08WhileDoWhile.js
@@ -0,0 +1,11 @@
+let inicio = 1
+while (inicio <=10){ //condição de repetição do bloco abaixo
+    console.log(inicio);
+    inicio++; //incrementa
+}
+
+let inicio2 = 10
+do { //executa o do ao menos 1x antes de testar a condição
+    console.log(inicio2);
+    inicio2++
+} while(inicio2<=10)
